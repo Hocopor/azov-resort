316f3781dc6925c293e90e2d6492c961575322b1
Hocopor <3dsmaxer@mail.ru>
2026-04-17 09:29:16 +0300

    1

--- app/src/middleware.ts
+++ app/src/middleware.ts
@@ -1,25 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { getToken } from 'next-auth/jwt'
 
-type SessionToken = {
-  role?: string
-}
-
-export async function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl
-  const token = (await getToken({ req })) as SessionToken | null
-
-  if (pathname.startsWith('/admin')) {
-    if (!token) {
-      return NextResponse.redirect(new URL('/auth/login?callbackUrl=/admin', req.url))
-    }
-  }
-
-  if (pathname.startsWith('/account') && !token) {
-    return NextResponse.redirect(new URL(`/auth/login?callbackUrl=${pathname}`, req.url))
-  }
-
+export function middleware(_req: NextRequest) {
   return NextResponse.next()
 }
 
